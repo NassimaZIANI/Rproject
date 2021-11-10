769690c7335e364452725439ddde61ed50863b6b
Marie Amaru <marie.amaru@1kmapied.com>
2021-11-10 12:47:52 +0100

    load csv

---
+++ PhysicsToolbox/ui.R
@@ -0,0 +1,34 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Physics Toolbox Project"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            fileInput("file1", "Choose CSV File",
+                      accept = c(
+                          "text/csv",
+                          "text/comma-separated-values,text/plain",
+                          ".csv")
+            ),
+            tags$hr(),
+            checkboxInput("header", "Header", TRUE)
+        ),
+        mainPanel(
+            tableOutput("contents")
+        )
+    )
+)
+)
